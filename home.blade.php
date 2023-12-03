63dbe44a31b85fcf91fc1d304306795adbd058c0
tyaraaaa <90371409+tyaraslw@users.noreply.github.com>
2023-12-03 17:24:53 +0700

    Add files via upload

--- home.blade.php
+++ home.blade.php
@@ -7,12 +7,20 @@
     <link rel="stylesheet" href="css/home.css">
     <title>home</title>
 </head>
+<style>
+    body{
+        background-color: #fff1ec; 
+    };
+
+</style>
 <body>
 @include('layouts.nav') 
     <br>
     <br>
     <br>
     <br>
+    <br>
      <h1 style="text-align:center;">{{$TextJudul}}</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
--- nav.blade.php
+++ nav.blade.php
@@ -1,5 +1,8 @@
-<nav class="navbar navbar-expand-lg fixed-top navbar-scroll shadow-0" style="background-color: #ffede7;">
-  <div class="container">
+<nav class="navbar navbar-expand-lg fixed-top navbar-scroll shadow-0" style="background-color: #ffede7;   box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.50)">
+<div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="/image/logo.png" alt="">
+              </a>
     <a class="navbar-brand"href="{{url('home')}}">Kasir</a>
     <button class="navbar-toggler ps-0" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarExample01"
       aria-controls="navbarExample01" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,18 +12,25 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarExample01">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item active">
-          <a class="nav-link px-3" href="#!">barang</a>
+      <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Produk
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="{{url('produk')}}">Data Produk</a></li>
+            <li><a class="dropdown-item" href="{{url('tambah_produk')}}">Tambah produk</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>  
+        <li class="nav-item">
+          <a class="nav-link px-3" href="{{url('')}}">Penjualan</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link px-3" href="#!">pembelian</a>
+          <a class="nav-link px-3" href="{{url('pelanggan')}}">Pelanggan</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link px-3" href="#!">stok</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link px-3" href="{{url('login')}}">login</a>
-        </li>
+        <!-- <li class="nav-item">
+          <a class="nav-link px-3" href="{{url('loginkasir')}}">login</a>
+        </li> -->
 
         <div class="d-flex">
           <a class="nav-link" href="{{url('logout')}}">
